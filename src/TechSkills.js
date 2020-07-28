7cdcdbce0a65d2aab6c0209dbeb35cc65e052e42
KieranTheMan <kieran9fowler@gmail.com>
2020-07-28 13:18:56 +0100

    create a Icons component, iconObj Array of Objects

--- src/TechSkills.js
+++ src/TechSkills.js
@@ -7,36 +7,50 @@ class TechSkills extends Component {
                     <h3>TECH SKILLS</h3>
                     <h2>What I Can Do</h2>
                     <div className='row'>
-                        <div>
-                            <span>
-                                <ion-icon name="logo-html5"></ion-icon>
-                            </span>
-                            <h4>HTML</h4>
-                            <p></p>
-                        </div>
-                        <div>
-                            <span>
-                                <ion-icon name="logo-css3"></ion-icon>
-                            </span>
-                            <h4>CSS3</h4>
-                            <p></p>
-                        </div>
-                        <div>
-                            <span>
-                                <ion-icon name="logo-javascript"></ion-icon>
-                            </span>
-                            <h4>JavaScript</h4>
-                            <p></p>
-                        </div>
-                        <div>
-                            <span>
-                                <ion-icon name="logo-react"></ion-icon>
-                            </span>
-                            <h4>React</h4>
-                            <p></p>
-                        </div>
+                        <>
+                            <Icons myicon={iconObj[0].icon} mytitle={iconObj[0].title}/>
+                            <Icons myicon={iconObj[1].icon} mytitle={iconObj[1].title}/>
+                            <Icons myicon={iconObj[2].icon} mytitle={iconObj[2].title}/>
+                            <Icons myicon={iconObj[3].icon} mytitle={iconObj[3].title}/>
+                        </>
                     </div>
                 </div>
+        );
+    }
+}
+
+const iconObj = [
+    {
+        icon: <ion-icon name="logo-html5"></ion-icon>,
+        title: 'HTML5'
+    },
+
+    {
+        icon:  <ion-icon name="logo-css3"></ion-icon>,
+        title: 'CSS3'
+    },
+
+    {
+        icon: <ion-icon name="logo-javascript"></ion-icon>,
+        title: 'JavaScript'
+    },
+
+    {
+        icon: <ion-icon name="logo-react"></ion-icon>,
+        title: 'react'
+
+    },
+]
+
+class Icons extends Component {
+    render() {
+        return (
+            <div>
+                <span>
+                    {this.props.myicon}
+                </span>
+                <h4>{this.props.mytitle}</h4>
+            </div>
         );
     }
 }
